f0c6c7b761115a647482dc460b8a98d33e244dd1
Ritwiza Tomar <106557993+Ritwiza5@users.noreply.github.com>
2023-11-15 17:32:21 +0530

    updating hero component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,19 +1,60 @@
 import "./hero.scss"
+import {motion} from "framer-motion";
 
+
+const textVariants={
+    initial:{
+        x:-500,
+        opacity:0,
+    },
+    animate:{
+        x:0,
+        opacity:1,
+    transition:{
+        duration:1,
+        staggerChildren:0.1,
+    }
+    },
+    scrollButton:{
+        opacity:0,
+        y:10,
+        transition:{
+            duration:2,
+            repeat:Infinity
+        }
+    }
+}
+const sliderVariants={
+    initial:{
+        x:0,
+    },
+    animate:{
+        x:"-220%",
+    transition:{
+        repeat:Infinity,
+        repeatType:"",
+        duration:20,
+        staggerChildren:0.1,
+    }
+    }
+}
 const Hero = () => {
   return (
     <div className="hero">
     <div className="wrapper">
-    <div className="textContainer">
-        <h2>RITWIZA TOMAR</h2>
-        <h1>FULLSTACK DEVELOPER</h1>
-        <div className="buttons">
-            <button>See the Latest Works</button>
-            <button>Contact Me</button>
-        </div>
-        <img src="/scroll.png" alt="" />
+    <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
+        <motion.h2  variants={textVariants}>RITWIZA TOMAR</motion.h2>
+        <motion.h1  variants={textVariants}>Fullstack developer</motion.h1>
+        <motion.div  variants={textVariants} className="buttons">
+            <motion.button  variants={textVariants}>See the Latest Works</motion.button>
+            <motion.button  variants={textVariants}>Contact Me</motion.button>
+        </motion.div>
+        <motion.img  variants={textVariants} animate="scrollButton" src="/scroll.png" alt="" />
+    </motion.div>
     </div>
-    </div>
+    <motion.div className="sliding" variants={sliderVariants} initial="initial" animate="animate">
+        Frontend  Backend  UI Designer  Problem Solver
+    </motion.div>
         <div className="imageContainer">
             <img src="hero.png" alt=" " />
         </div>
