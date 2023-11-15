814c0228cf7130d943428551ecc3551ab68eb287
Ritwiza Tomar <106557993+Ritwiza5@users.noreply.github.com>
2023-11-15 14:43:47 +0530

    toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+ import "./app.scss"
+ import Navbar from "./components/navbar/Navbar";
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section id="Homepage"><Navbar/></section>
+    <section id="Skills">Parallax</section>
+    <section >Services</section>
+    <section id="Projects">Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id="Contact">Contact</section>
+  </div>;
 };
 
 export default App;
